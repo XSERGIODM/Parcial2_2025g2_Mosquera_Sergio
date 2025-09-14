371211b7ccedf55e4aa316f76db403e4bdf2497a
Sergio <sergiodanilomosqueraquigua@gmail.com>
2025-09-14 12:42:33 -0500

    Crea encabezado y pie de página con navegación mejorada
    Agrega componentes de navbar y footer con enlaces de contacto y redes sociales, utilizando estilos Bootstrap e íconos. Mejora la navegación resaltando el enlace activo y usando el router de Angular.
    
    Moderniza la estructura visual y facilita el acceso a secciones clave. Integra Bootstrap Icons en la aplicación.

--- src/app/components/nav/nav.ts
+++ src/app/components/nav/nav.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import {Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './nav.html',
   styleUrl: './nav.css'
 })
 export class Nav {
-
+    routerLinkActive = signal('home');
+    routerLinkActiveClass = signal('active');
+  constructor(private router: Router){
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.routerLinkActive.set(event.urlAfterRedirects.split('/')[1]);
+      }
+    });
+  }    
 }
