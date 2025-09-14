5cb0121bb55054ad9b3eefbf5a96ab8fc06d90c5
Sergio <sergiodanilomosqueraquigua@gmail.com>
2025-09-14 12:02:33 -0500

    Simplifica la estructura y define rutas principales
    Reemplaza el contenido placeholder de la plantilla principal por una estructura básica con barra de navegación y pie de página.
    Actualiza la configuración de rutas incluyendo páginas de inicio, about y proyectos, facilitando la navegación en la app.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { AboutPagina } from './pages/about-pagina/about-pagina';
+import { ProyectosPagina } from './pages/proyectos-pagina/proyectos-pagina';
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home
+    },
+    {
+        path: 'home',
+        component: Home
+    },
+    {
+        path: 'about',
+        component: AboutPagina
+    },
+    {
+        path: 'proyectos',
+        component: ProyectosPagina
+    }
+];
